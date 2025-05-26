0007f91c42667604e0bede240526b09b36133aae
PedroHenrique2204 <pedrorft22@gmail.com>
2025-05-26 08:23:36 -0300

    Add files via upload

---
+++ aplicacao.js
@@ -0,0 +1,5 @@
+/* Não consegui fazer o Java funcionar, quando for pedir ao chat gpt, 
+pega os codigos do index.html e style.css e pede para ele criar um script em java
+que faça a integração das páginas do cadastro.html, topologia.html e testederede.html
+para que o botão da primeira pagina ecrito "cadastrar-se" 
+funcione e as paginas estejam linkadas */
